0e22b05152b34a75ee528ba9a689e4f533cb376a
Lee.t.c <seonghoo1217@naver.com>
2022-05-14 22:00:29 +0900

    20220514

---
+++ src/programers/level1/음양더하기.java
@@ -0,0 +1,14 @@
+package programers.level1;
+
+public class 음양더하기 {
+    class Solution {
+        public int solution(int[] absolutes, boolean[] signs) {
+            int answer = 0;
+            for (int i=0;i<absolutes.length;i++){
+                if (signs[i]==false) answer-=absolutes[i];
+                else answer+=absolutes[i];
+            }
+            return answer;
+        }
+    }
+}
