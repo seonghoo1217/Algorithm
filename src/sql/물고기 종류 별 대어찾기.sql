68a5021f63bce267c2860477b079d30161a4efdd
seonghoo1217 <lsh6451217@gmail.com>
2024-11-01 20:20:50 +0900

    [Programmers] 물고기 종류 별 대어찾기

---
+++ src/sql/물고기 종류 별 대어찾기.sql
@@ -0,0 +1,9 @@
+SELECT fi.ID, fn.FISH_NAME, fi.LENGTH
+FROM FISH_INFO fi
+         JOIN FISH_NAME_INFO fn ON fi.FISH_TYPE = fn.FISH_TYPE
+WHERE fi.LENGTH IS NOT NULL
+  AND fi.LENGTH = (SELECT MAX(LENGTH)
+                   FROM FISH_INFO
+                   WHERE FISH_TYPE = fi.FISH_TYPE
+                     AND LENGTH IS NOT NULL)
+ORDER BY fi.ID ASC;
