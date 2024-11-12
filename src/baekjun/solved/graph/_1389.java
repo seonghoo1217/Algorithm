473ab04559cb06c356247dbe0ec07de2297d4620
seonghoo1217 <lsh6451217@gmail.com>
2024-11-12 22:21:33 +0900

    [Baekjoon] 1389

---
+++ src/baekjun/solved/graph/_1389.java
@@ -0,0 +1,65 @@
+package baekjun.solved.graph;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _1389 {
+    static final int INF = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = INF;
+
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            arr[x][y] = arr[y][x] = 1;
+        }
+
+        // 플로이드 와샬 알고리즘
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    // 최단경로 초기화
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+
+        int res = INF;
+        int idx = -1;
+
+        for (int i = 1; i <= N; i++) {
+            int total = 0;
+            for (int j = 1; j <= N; j++) {
+                total += arr[i][j];
+            }
+
+            if (res > total) {
+                res = total;
+                idx = i;
+            }
+        }
+
+        System.out.println(idx);
+    }
+}
