8a2d3ba465023cac8069943da886d43f973414c1
seonghoo1217 <lsh6451217@gmail.com>
2024-12-26 21:01:19 +0900

    Time_Conversion

---
+++ src/buzzvil/Time_Conversion.java
@@ -0,0 +1,52 @@
+package buzzvil;
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String timeConversion(String s) {
+        // Write your code here
+        String target = s.substring(0, s.length() - 2);
+        String period = s.substring(s.length() - 2);
+
+        String[] parts = target.split(":");
+        int hour = Integer.parseInt(parts[0]);
+
+        if (period.equals("AM")) {
+            if (hour == 12) {
+                hour = 0;
+            }
+        } else {
+            if (hour != 12) {
+                hour += 12;
+            }
+        }
+
+        return String.format("%02d:%s:%s", hour, parts[1], parts[2]);
+    }
+
+}
+
+public class Time_Conversion {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.timeConversion(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
